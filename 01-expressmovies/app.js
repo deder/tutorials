9603927fcb899b081f43ed7ac84be4a1fb8ccf09
deder <fcosta@deder.fr>
2018-05-28 21:35:02 +0200

    Création du model mongoDb

--- 01-expressmovies/app.js
+++ 01-expressmovies/app.js
@@ -5,26 +5,49 @@ const expressJwt = require('express-jwt');
 const configServer = require('./config/expressConfig');
 const dbConfig = require('./config/dbConfig');
 const mongoose = require('mongoose');
+const faker = require('faker');
 const app = express();
-
 const upload = multer();
+
+faker.locale = "fr";
+
 //const urlEncodedParser = configServer.urlEncodedParser;
 const fakeUser = {email:"deder@deder.fr", password: "PassWord", user :"deder"};
 console.log(`${dbConfig.protocol}${dbConfig.login}:${dbConfig.password}@${dbConfig.url}`);
 const db = mongoose.connection;
-mongoose.connect(`${dbConfig.protocol}${dbConfig.login}:${dbConfig.password}@${dbConfig.url}`);
+mongoose.connect(`${dbConfig.protocol}${dbConfig.url}`);
 //const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: cannot connect'));
 db.once('open', () => {
     console.log('connected to the DB :)');
 });
-let frenchMovies = [
-    {title:`Larguées`, year:`2018`},
-    {title:`Le Fabuleux Destin d'Amélie POULAIN`, year:`2001`},
-    {title:`Léon`, year:`1994`},
-    {title:`Bienvenue chez les Ch'tis`, year:`2008`},
-    {title:`La grande vadrouille`, year:`1966`}
-];
+
+const movieSchema = mongoose.Schema({
+    movieTitle: String,
+    movieYear: Number
+});
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+/*
+const title = faker.lorem.sentence(3);
+const year = Math.floor(Math.random() * 80) + 1950;
+
+const myMovie = new Movie({
+    movieTitle: title,
+    movieYear: year
+});
+
+myMovie.save((err, savedMovie) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("savedMovie", savedMovie);
+    }
+});
+*/
+
+let frenchMovies = [];
 
 var user = {
     login: "fcosta",
@@ -44,7 +67,7 @@ var url = {
 
 
 app.use(url.staticContent , express.static('public'));
-app.use(expressJwt({secret:configServer.secret}).unless({path: [url.root, url.connect, url.movies, url.addMovie, , url.searchMovie]}));
+app.use(expressJwt({secret:configServer.secret}).unless({path: [url.root, url.connect, url.movies, url.addMovie, , url.searchMovie, new RegExp('/movies.*/', 'i')]}));
 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
@@ -55,7 +78,18 @@ app.get(url.root, (req, res) => {
 
 app.get(url.movies, (req, res) => {
     const title = `Film Français des trentes dernieres années`;
-    res.render('movies', {frenchMovies, title});
+
+    frenchMovies = [];
+    Movie.find((err, movies) =>{
+        if(err){
+            console.error(err);
+            res.sendStatus(500);
+        }else{
+            frenchMovies = movies;
+            res.render('movies', {frenchMovies, title});
+        }
+    });
+    
 });
 
 app.post(url.movies, upload.fields([]), (req, res) => {
@@ -63,20 +97,32 @@ app.post(url.movies, upload.fields([]), (req, res) => {
         res.sendStatus(500);
     }else{
         const formData  = req.body;
-        const newMovie = {title:req.body.movieTitle, year:req.body.movieYear}
-        frenchMovies = [...frenchMovies, newMovie];
-        console.log(frenchMovies);
-        res.sendStatus(201);
+        const myMovie = new Movie({
+            movieTitle:formData.movieTitle,
+            movieYear:formData.movieYear
+        });
+        myMovie.save((err, savedMovie) =>{
+            if(err){
+                console.error(err);
+                res.sendStatus(500);
+            }else{
+                console.log("savedMovie", savedMovie);
+                res.sendStatus(201);
+            }
+
+        })
     }
 });
 
-app.get(url.addMovie, (req, res) => {
-    res.send(`page permettant d'ajouter un film`);
-});
 
 app.get(url.selectMovie, (req, res) => {
     const id = req.params.id;
     res.render('movie-details', { movieId: id});
+});
+
+app.put("/movies/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Put request to movie of id ${id}`);
 });
 
 app.get(url.searchMovie, (req, res) => {
--- 01-expressmovies/config/dbConfig.js
+++ 01-expressmovies/config/dbConfig.js
@@ -1,8 +1,8 @@
 // dbConfig.js
 // =========
 module.exports = {
-    login:"fcosta",
-    password:'dederUser',
-    url:"ds233320.mlab.com:33320/expressmovies",
+    login:"",
+    password:'',
+    url:"localhost/expressmovies",
     protocol:"mongodb://"
 };
