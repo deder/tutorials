39baf29eb44c302f405b75dbb10562d6a9b3ca60
fcosta <fcosta@deder.fr>
2018-08-08 09:53:32 +0200

    [Projet snake] Intégration du transcoder Babel

--- 02-snake/webpack.config.js
+++ 02-snake/webpack.config.js
@@ -8,5 +8,19 @@ module.exports = {
     devServer:{
         contentBase: path.resolve(__dirname, "dist"),
         open:true
+    }, 
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options:{
+                        presets: ["env"]
+                    }
+                }
+            }
+        ]
     }
 }
