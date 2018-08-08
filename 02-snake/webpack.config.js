bbb3301cf8ccf2a515c1584f50f6d2ca43808755
fcosta <fcosta@deder.fr>
2018-08-08 09:44:24 +0200

    [Projet snake] Ajout et initialisation de webpack-dev-server

--- 02-snake/webpack.config.js
+++ 02-snake/webpack.config.js
@@ -4,5 +4,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename:"bundle.js"
+    },
+    devServer:{
+        contentBase: path.resolve(__dirname, "dist"),
+        open:true
     }
 }
